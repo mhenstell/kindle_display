eafc31568e274137cdd7d7ae456a19f16a4c1092
Max Henstell <max@blip.tv>
2012-10-05 15:48:32 -0400

    added wunderground, rain, snow percents

--- server/weather-script.py
+++ server/weather-script.py
@@ -8,15 +8,25 @@ import urllib2
 from xml.dom import minidom
 import datetime
 import codecs
+import json
 
+api_key = ""
+zip_code = 11233
+wunderground_current_url = "http://api.wunderground.com/api/%s/conditions/q/%s.json" % (api_key, zip_code)
 
+wunderground_current_data = json.loads(urllib2.urlopen(wunderground_current_url).read())
+lat = wunderground_current_data['current_observation']['observation_location']['latitude']
+lon = wunderground_current_data['current_observation']['observation_location']['longitude']
+observed_temp = '%.1f' % wunderground_current_data['current_observation']['temp_f']
 
-#
-# Download and parse weather data
-#
+wunderground_forecast_url = "http://api.wunderground.com/api/%s/forecast/q/%s.json" % (api_key, zip_code)
+wunderground_forecast_data = json.loads(urllib2.urlopen(wunderground_forecast_url).read())
 
-# Fetch data (change lat and lon to desired location)
-weather_xml = urllib2.urlopen('http://graphical.weather.gov/xml/SOAP_server/ndfdSOAPclientByDay.php?whichClient=NDFDgenByDay&lat=39.3286&lon=-76.6169&format=24+hourly&numDays=4&Unit=e').read()
+pop = wunderground_forecast_data['forecast']['simpleforecast']['forecastday'][0]['pop']
+
+NWS_url = "http://graphical.weather.gov/xml/SOAP_server/ndfdSOAPclientByDay.php?whichClient=NDFDgenByDay&lat=%s&lon=%s&format=24+hourly&numDays=4&Unit=e" % (lat, lon)
+
+weather_xml = urllib2.urlopen(NWS_url).read()
 dom = minidom.parseString(weather_xml)
 
 # Parse temperatures
@@ -33,18 +43,18 @@ for item in xml_temperatures:
         for i in range(len(values)):
             lows[i] = int(values[i].firstChild.nodeValue)
 
-pop = dom.getElementsByTagName('probability-of-precipitation')
-for item in pop:
-	values = item.getElementsByTagName('value')
+# pop = dom.getElementsByTagName('probability-of-precipitation')
+# for item in pop:
+# 	values = item.getElementsByTagName('value')
 	#for i in range(len(values)):
 		#print values[i].firstChild.nodeValue
 
 
-time_layout = dom.getElementsByTagName('time-layout')
-for item in time_layout:
-	values = item.getElementsByTagName('summarization')
-	for i in range(len(values)):
-		print values[i].firstChild.nodeValue
+# time_layout = dom.getElementsByTagName('time-layout')
+# for item in time_layout:
+# 	values = item.getElementsByTagName('summarization')
+# 	for i in range(len(values)):
+# 		print values[i].firstChild.nodeValue
 
 
 # Parse icons
@@ -59,6 +69,8 @@ day_one = datetime.datetime.strptime(xml_day_one, '%Y-%m-%d')
 
 
 
+print icons
+
 #
 # Preprocess SVG
 #
@@ -70,6 +82,11 @@ output = codecs.open('weather-script-preprocess.svg', 'r', encoding='utf-8').rea
 output = output.replace('ICON_ONE',icons[0]).replace('ICON_TWO',icons[1]).replace('ICON_THREE',icons[2]).replace('ICON_FOUR',icons[3])
 output = output.replace('HIGH_ONE',str(highs[0])).replace('HIGH_TWO',str(highs[1])).replace('HIGH_THREE',str(highs[2])).replace('HIGH_FOUR',str(highs[3]))
 output = output.replace('LOW_ONE',str(lows[0])).replace('LOW_TWO',str(lows[1])).replace('LOW_THREE',str(lows[2])).replace('LOW_FOUR',str(lows[3]))
+output = output.replace('CURRENT', observed_temp)
+output = output.replace('RAIN_BOOL', '').replace('RAIN_PERC', '')
+output = output.replace('SNOW_BOOL', '').replace('SNOW_PERC', '')
+
+
 
 # Insert days of week
 one_day = datetime.timedelta(days=1)
