bf16479767034cb806e5bbcef8205b8e4f95b83b
Max Henstell <max@blip.tv>
2012-10-05 18:04:22 -0400

    parameterizing display-weather.sh

--- kindle/display-weather.sh
+++ kindle/display-weather.sh
@@ -3,10 +3,10 @@
 cd "$(dirname "$0")"
 
 rm weather-script-output.png
-eips -c
-eips -c
+#eips -c
+#eips -c
 
-if wget http://server/path/to/weather-script-output.png; then
+if wget $1; then
 	eips -g weather-script-output.png
 else
 	eips -g weather-image-error.png
